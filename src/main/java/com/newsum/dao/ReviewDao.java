b7434df4329d17baf334c179a13f8879fe1288bc
Micah Newsum <mnewsum@glenair.com>
2021-02-21 18:26:58 -0800

    Create ReviewDao.java

---
+++ src/main/java/com/newsum/dao/ReviewDao.java
@@ -0,0 +1,14 @@
+package com.newsum.dao;
+
+import com.newsum.exc.DaoException;
+import com.newsum.model.Review;
+
+import java.util.List;
+
+public interface ReviewDao {
+  void add(Review review) throws DaoException;
+
+  List<Review> findAll();
+
+  List<Review> findByCourseId();
+}
