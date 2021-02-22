ef802e13fba50ce9c15e3fec9edf10376bfd391d
Micah Newsum <mnewsum@glenair.com>
2021-02-21 19:26:46 -0800

    Update access modifer of class and methods so tests can execute.

--- src/test/java/com/newsum/dao/Sql2oCourseDaoTest.java
+++ src/test/java/com/newsum/dao/Sql2oCourseDaoTest.java
@@ -1,20 +1,48 @@
 package com.newsum.dao;
 
-import org.junit.Before;
-import org.junit.Test;
+    import static org.junit.Assert.assertNotEquals;
 
-class Sql2oCourseDaoTest {
+    import com.newsum.model.Course;
+    import org.junit.After;
+    import org.junit.Before;
+    import org.junit.Test;
+    import org.sql2o.Connection;
+    import org.sql2o.Sql2o;
+
+public class Sql2oCourseDaoTest {
+
+  private Sql2oCourseDao dao;
+  private Connection con;
 
   @Before
-  void setUp() {
-    String connectionString = "jdbc:h2:mem:testing";
+  public void setUp() {
+    String connectionString = "jdbc:h2:mem:testing;INIT=RUNSCRIPT from 'classpath:db/init.sql'";
+    Sql2o sql2o = new Sql2o(connectionString,"","");
+    dao = new Sql2oCourseDao(sql2o);
+    // keep connection open so that database is not wiped out
+    con = sql2o.open();
+  }
+
+  @After
+  public void name() {
+    con.close();
   }
 
   @Test
-  void add() {
+  public void addingCourseSetsId() throws Exception{
+    Course course = new Course("Test","http://test.com");
+    int origCourseId = course.getId();
+
+    dao.add(course);
+
+    assertNotEquals(origCourseId,course.getId());
   }
 
   @Test
-  void findAll() {
+  public void findAll() {
+
   }
+
 }
+
+
