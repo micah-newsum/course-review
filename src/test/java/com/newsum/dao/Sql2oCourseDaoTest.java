a8c715e7040301174ff11bb5d72862aa83283abe
Micah Newsum <mnewsum@glenair.com>
2021-02-21 18:27:19 -0800

    Create Sql2oCourseDaoTest.java

---
+++ src/test/java/com/newsum/dao/Sql2oCourseDaoTest.java
@@ -0,0 +1,20 @@
+package com.newsum.dao;
+
+import org.junit.Before;
+import org.junit.Test;
+
+class Sql2oCourseDaoTest {
+
+  @Before
+  void setUp() {
+    String connectionString = "jdbc:h2:mem:testing";
+  }
+
+  @Test
+  void add() {
+  }
+
+  @Test
+  void findAll() {
+  }
+}
