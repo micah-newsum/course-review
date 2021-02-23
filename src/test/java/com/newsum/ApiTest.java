6a5d74982850f4e6105a6cd560599c422ae82ad9
Micah Newsum <mnewsum@glenair.com>
2021-02-23 10:21:44 -0800

    Implement functional tests for reviews end points.

--- src/test/java/com/newsum/ApiTest.java
+++ src/test/java/com/newsum/ApiTest.java
@@ -6,7 +6,9 @@ import static spark.Spark.stop;
 import com.google.gson.Gson;
 
 import com.newsum.dao.Sql2oCourseDao;
+import com.newsum.dao.Sql2oReviewDao;
 import com.newsum.model.Course;
+import com.newsum.model.Review;
 import com.newsum.testing.ApiClient;
 import com.newsum.testing.ApiResponse;
 import org.junit.After;
@@ -17,7 +19,9 @@ import org.junit.Test;
 import org.sql2o.Connection;
 import org.sql2o.Sql2o;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
 public class ApiTest {
@@ -28,6 +32,7 @@ public class ApiTest {
   private ApiClient client;
   private Gson gson;
   private Sql2oCourseDao courseDao;
+  private Sql2oReviewDao reviewDao;
 
   @BeforeClass
   public static void startServer() {
@@ -46,6 +51,7 @@ public class ApiTest {
         sql2o =
         new Sql2o(TEST_DATASOURCE + ";INIT=RUNSCRIPT from 'classpath:db/init.sql'", "", "");
     courseDao = new Sql2oCourseDao(sql2o);
+    reviewDao = new Sql2oReviewDao(sql2o);
     conn = sql2o.open();
     client = new ApiClient("http://localhost:" + PORT);
     gson = new Gson();
@@ -85,5 +91,56 @@ public class ApiTest {
   public void missingCoursesReturnNotFoundStatus() {
     ApiResponse res = client.request("GET","/courses/42");
     assertEquals(404,res.getStatus());
+  }
+
+  @Test
+  public void addingReviewReturnsCreatedStatusCode() throws Exception{
+    Course course = newTestCourse();
+    courseDao.add(course);
+
+    Map<String, Object> values = new HashMap<>();
+    values.put("rating", 5);
+    values.put("comment", "test comment");
+    values.put("courseId",course.getId());
+
+    ApiResponse response = client.request("POST", String.format("/courses/%d/reviews",course.getId()), gson.toJson(values));
+    assertEquals(201, response.getStatus());
+  }
+
+  @Test
+  public void addingReviewToNonexistentCourseReturnsErrorStatusCode() throws Exception{
+    // set course id that does not exist
+    int courseId = 100;
+
+    // create mock review
+    Map<String, Object> values = new HashMap<>();
+    values.put("rating", 5);
+    values.put("comment", "test comment");
+    values.put("courseId",100);
+
+    // submit api request
+    ApiResponse response = client.request("POST", String.format("/courses/%d/reviews",courseId), gson.toJson(values));
+
+    // run test
+    assertEquals(500, response.getStatus());
+  }
+
+  @Test
+  public void multipleReviewsReturnedForCourse() throws Exception{
+    // create course
+    Course course = newTestCourse();
+    courseDao.add(course);
+
+    // create reviews for course
+    Review firstReview = new Review(course.getId(),5,"Test comment");
+    Review secondReview = new Review(course.getId(),1,"Test comment");
+
+    // create reviews and persist to database
+    reviewDao.add(firstReview);
+    reviewDao.add(secondReview);
+
+    ApiResponse response = client.request("GET", String.format("/courses/%d/reviews",course.getId()));
+    List<Review> reviews = gson.fromJson(response.getBody(),List.class);
+    assertEquals(2, reviews.size());
   }
 }
